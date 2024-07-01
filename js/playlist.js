e19657abf382b8064be72ce88d1952e82d1a4361
richard233n <1943126663@qq.com>
2024-07-01 23:41:57 +0800

    Auto Update

---
+++ js/playlist.js
@@ -0,0 +1,26 @@
+let songs = [
+  {
+    name: " Премьера",
+    artist: "Adam | Zhurek",
+    img: "img1",
+    audio: "music1"
+  },
+  {
+    name: " 安和橋",
+    artist: "宋冬野",
+    img: "img2",
+    audio: "music2"
+  },
+  {
+    name: "我用什么把你留住",
+    artist: "福禄寿",
+    img: "img3",
+    audio: "music3"
+  },
+  {
+    name: "牧馬城市",
+    artist: "毛不易",
+    img: "img4",
+    audio: "music4"
+  }
+]
